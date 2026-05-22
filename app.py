fda2b3e455957244cd0434dce6030c90c95163f2
Jonferuiz <ironkit1@gmail.com>
2026-05-21 20:31:42 -0500

    Suma de dos Numeros

---
+++ app.py
@@ -0,0 +1,11 @@
+# Pedir al usuario que ingrese el primer número
+numero1 = float(input("Ingresa el primer número: "))
+
+# Pedir al usuario que ingrese el segundo número
+numero2 = float(input("Ingresa el segundo número: "))
+
+# Sumar los dos números
+suma = numero1 + numero2
+
+# Mostrar el resultado por pantalla
+print("La suma de", numero1, "y", numero2, "es:", suma)
